cc61fc316bb7836fb8f33f3847a60fc891f8e57d
Yashvi Jasoliya <yashvijasoliya42@gmail.com>
2023-10-16 15:04:48 +0530

    add

---
+++ ps_12_pr_3.java
@@ -0,0 +1,31 @@
+
+package folder.foldelr1.folderl2;
+
+// pr 3
+
+class calculator{
+    public void calculate(int a, int b){
+       System.out.println("Your result is :- " + a+b); 
+    }
+}
+
+class Sccalculator{
+    public void calculate(int a, int b){
+       System.out.println("Your result is :- " + Math.sin(a+b)); 
+    }
+}
+
+class Hycalculator{
+    public void calculate(int a, int b){
+        System.out.println("Your result is :- " + a+b); 
+        System.out.println("Your result is :- " + Math.sin(a+b)); 
+    }
+}
+
+ 
+public class ps_12_pr_3 {
+    public static void main(String[] args) {
+        System.out.println("I am main method");
+    }
+    
+}
